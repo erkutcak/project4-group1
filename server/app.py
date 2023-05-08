42321014974e68ec65877ed151d0062762fa9e44
Ohskakatelynn <ohskakatelynn@gmail.com>
2023-05-08 09:36:21 -0400

    rest of app.py

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from config import app, db, api
-from models import User, ShoppingCart
+from models import User, ShoppingCart, Transaction
 # from models import User, Recipe
 
 app = Flask(__name__)
@@ -67,7 +67,67 @@ class ShoppingCart(Resource):
 
 api.add_resource(ShoppingCart, '/shoppingcarts')
 
+class TransactionsById(Resource):
+    def get(self, id):
+        t = Transaction.query.filter_by(id = id).first()
+        if t == None:
+            return make_response({'error': 'no Transactions'}, 404)
+        return make_response(t.to_dict(), 200)
+    def delete(self, id):
+        t = Transaction.query.filter_by(id = id).first()
+        db.session.delete(t)
+        db.session.commit()
+        response_body = {
+            "deleted successfully": True,
+            "message": "Transaction deleted successfully"
+        }
+        response = make_response(
+            response_body,
+            202
+        )
+        return response
+    def patch(self, id):
+        try:
+            t = Transaction.query.filter_by(id = id).first()
 
+            for attr in request.get_json():
+                setattr(t, attr, request.get_json()[attr])
+        except:
+            response_body = {
+                'error': 'no transaction'
+            }
+            return make_response( response_body, 404 )
+        else:
+            db.session.add(t)
+            db.session.commit()
+        
+        return make_response(t.to_dict(), 200)
+    
+api.add_resource(TransactionsById, '/transactions/<int:id>')
+
+class Transactions(Resource):
+    def get(self):
+        t_list = [t.to_dict() for t in Transaction.query.all()]
+        if len(t_list) == 0:
+            return make_response({'error': 'no Transactions'}, 404)
+        return make_response(t_list, 200)
+    def post (self):
+        data = request.get_json()
+        newTransaction = Transactions(
+            id= data["id"],
+            seller_id = data["seller_id"], 
+            buyer_id = data["buyer_id"],
+            item_id = data["item_id"]
+            )
+        try:
+            db.session.add(newTransaction)
+            db.session.commit()
+            return make_response (newTransaction.to_dict(), 200)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({'error': f'{repr(e)}'}, 422)
+        
+api.add_resource(Transactions, '/transactions')
 
 # Local imports
 
