5d41e934fee71168e53feffac6fcd561c5654126
Ohskakatelynn <ohskakatelynn@gmail.com>
2023-05-11 09:06:14 -0400

    k

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -15,12 +15,15 @@ function HomePage({user, onLogin}) {
     return (
         <div className="header">
             <NavBar user={user} onLogin={onLogin}/>
-            <h1>Featured Items</h1>
+            <h1 className="center">Featured Items</h1>
+            <h1 className="categories">Category</h1>
             <div className="grid-container">
                 {items.map((item) => (
                     <div key={item.id} className="card">
+                        <img>{item.image}</img>
                         <h3>{item.name}</h3>
                         <p>Price: {item.price}</p>
+                        <button className="cardbutton" type="submit">Add To Cart</button>
                     </div>
                 ))}
             </div>
