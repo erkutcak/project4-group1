5b28a8636547f24f70756fb47df63327237a1aaf
Reed <reedbroadhead@gmail.com>
2023-05-14 19:22:39 -0500

    completes transactions

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import logo from "../images/logo.png";
 import "../Transactions.css"
 import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
@@ -25,18 +26,21 @@ function Transactions({user, item}) {
 
     const displayItems = transactions.map(el => {
         const currentItem = item.filter(item => item.id === el.item_id)
-        console.log(currentItem[0].image);
+        // console.log(transactions);
+        // console.log(transactions[0].item_id)
+        // console.log(transactions.filter(el => el.item_id == currentItem[0].id)[0].created_at)
+        // console.log(transactions.filter(el => el.item_id == currentItem.id));
         return (
             <li className="transactions-card">
-                <h3 className="transaction-img">{currentItem[0].image}</h3>
+                <img className="transaction-img" src={currentItem[0].image ? currentItem[0].image : logo}></img>
                 <h3 className="transaction-name">Item: {currentItem[0].name}</h3>
                 <h3 className="transaction-price">Price: ${currentItem[0].price}</h3>
                 <h3 className="transaction-seller">Seller: {users?.filter(x => x.id === el.seller_id)[0].username}</h3>
-                <h3 className="transaction-date">Date: {transactions[0].created_at}</h3>
+                <h3 className="transaction-date">Date: {transactions.filter(el => el.item_id == currentItem[0].id)[0].created_at}</h3>
                 {/* <h3 className="transaction-item-id">Item id: {el.item_id}</h3> */}
             </li>)
-    })
-
+    }) 
+// transactions[0].created_at
     return (
         <div className='transactions-container'>
             <div className="transactions-left">
